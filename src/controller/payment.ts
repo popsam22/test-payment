0d1ad0ee4e2523b1a58acb9067559b0f33755ffd
ZenkleusLimited <skoolbod@gmail.com>
2024-09-21 09:27:23 +0100

    fix: server logs

--- src/controller/payment.ts
+++ src/controller/payment.ts
@@ -50,9 +50,10 @@ export const webhook = (req: Request, res: Response) => {
   try {
     if (eventData.event === "charge.success") {
       const transactionId = eventData.data.id;
-      return res
-        .status(200)
-        .json({ message: `Transaction ${transactionId} was successful` });
+      console.log(`Transaction ${transactionId} was successful`);
+      // return res
+      //   .status(200)
+      //   .json({ message: `Transaction ${transactionId} was successful` });
     }
   } catch (error) {
     return res.status(500).json({ error });
