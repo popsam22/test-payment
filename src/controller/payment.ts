18608aade3595fa981e7bb9e0037dab763fceb0c
popsam22 <97094431+popsam22@users.noreply.github.com>
2025-02-13 09:21:46 +0100

    Update payment.ts

--- src/controller/payment.ts
+++ src/controller/payment.ts
@@ -6,15 +6,17 @@ import { Request, Response } from "express";
 dotenv.config();
 const API_SECRET_KEY = process.env.SECRET_KEY;
 
-const verify = (
-  eventData: any,
-  signature: string | string[] | undefined
-): boolean => {
-  const hmac = crypto.createHmac("sha512", API_SECRET_KEY!);
-  const expectedSignature = hmac
-    .update(JSON.stringify(eventData))
-    .digest("hex");
-  return expectedSignature === signature;
+const verify = (transactionData,signature) => {
+  try {
+    const hmac = crypto.createHmac("sha512", process.env.MYSECRETKEY);
+    const expectedSignature = hmac
+      .update(JSON.stringify(transactionData))
+      .digest("hex");
+    return expectedSignature === signature;
+  } catch (error) {
+    console.error('Signature verification failed:', error);
+    return false;
+  }
 };
 
 export const initiateTransaction = async (req: Request, res: Response) => {
@@ -41,17 +43,16 @@ export const initiateTransaction = async (req: Request, res: Response) => {
 };
 
 export const webhook = (req: Request, res: Response) => {
-  const eventData = req.body;
+  const transactionData = req.body;
   const signature = req.headers["x-paystack-signature"];
-  if (!verify(eventData, signature)) {
+  if (!verify(transactionData, signature)) {
     return res.status(400).json({ message: "Signature not verified" });
   }
-
   try {
-    if (eventData.event === "charge.success") {
-      const transactionId = eventData.data.id;
+    if (transactionData.event === "charge.success") {
+      const transactionId = transactionData.data.reference;
       console.log(`Transaction ${transactionId} was successful`);
-      return res.status(200);
+      return res.status(200).json({message: "Payment received succesffully"});
     }
   } catch (error) {
     return res.status(500).json({ error });
